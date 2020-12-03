3cae591839b1fb796c3b8e72a0275919068f8338
Liam <paretoyxe@gmail.com>
2020-12-03 17:24:26 -0600

    Added init function for the player object

--- player.py
+++ player.py
@@ -2,8 +2,21 @@ import pygame
 
 
 
+
+
+
+
+
 player = {"xLocation":1,"yLocation":1,"tileX":1,"tileY":1}
-playerImage = pygame.image.transform(pygame.image.load('tree.png'),(tileX,tileY))
+playerImage = null
+
+def playerInit(xlocation,yLocation,tileX,tileY):
+	player["xLocation"] = xLocation
+	player["yLocation"] = yLocation
+	player["tileX"] = tileX
+	player["tileY"] = tileY
+
+	playerImage = pygame.transform.scale(pygame.image.load('tree.png'),(player["tileX"],player["tileY"]))
 
 
 def renderPlayer(gameSurface):
